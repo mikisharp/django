552b5969574a521b3edc9ef69f55094d86d8218f
Mike <mikehezron@gmail.com>
2023-09-14 19:52:05 +0300

    Added form

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -5,5 +5,7 @@ app_name="posts"
 
 urlpatterns = [
     path('', views.home, name="home_page"),
-    path('about/', views.about, name="about_page")
+    path('about/', views.about, name="about_page"),
+    path('post/<int:post_id>', views.post_details, name="post_details"),
+    path('create_post/', views.create_post, name="create_post")
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -13,9 +13,27 @@ def home(request):
     }
     
     return render(request, 'home/landingpage.html', context)
-
+ 
 def about(request):
     context = {
         'title': 'About',
     }
     return render(request, 'about/about_page.html', context)
+
+def post_details(request, post_id):
+    post = Post.objects.filter(id=post_id).first()
+    print(post)
+   
+    context = {
+        "post": post,
+        "title": post.title
+    }
+    return render(request, 'post/single_post.html', context)
+
+def create_post(request):
+    context = {
+        'title': "Add Post"
+        
+    }
+    return render(request, 'post/add_post.html')
+
